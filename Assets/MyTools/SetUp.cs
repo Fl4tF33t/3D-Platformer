00efc34d9e0d8d286761b3bdb7613623963aa80f
Khairulhelmy Hussain <ab5823@student.jamk.fi>
2025-05-23 15:07:59 +0300

    Project init

---
+++ Assets/MyTools/SetUp.cs
@@ -0,0 +1,11 @@
+public static class SetUp {
+    public static void CreateDefaultFolders() {
+        
+    }
+
+    private static class Folders {
+        public static void CreateDefault(string root, params string[] folders) {
+            
+        }
+    }
+}
